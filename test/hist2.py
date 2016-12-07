b0906dd154c90f62871fcd4a0175a734b19af850
Ashley Parker <ash.marie.parker@gmail.com>
2016-12-07 11:32:50 -0600

    finalizing hist2.py

--- test/hist2.py
+++ test/hist2.py
@@ -1,12 +1,13 @@
 
 
 import ROOT
+import array as a
 
 theOutfile=ROOT.TFile("outfile.root","RECREATE")
 
 
 # Bins are defined by an array 
-ptBs =  array.array('d', [200., 300., 400., 500., 800.])
+ptBs =  a.array('d', [200, 300 , 400 , 500 , 800 ])
 nptBs = len(ptBs) - 1
 
 # Define a binned histogram for storing the peak of the gaussian fit to a Top mass spectrum in each Pt bin
@@ -17,23 +18,24 @@ hpeak = ROOT.TH1F("hpeak", " ;p_{T} of jet (GeV); Peak Location ",  nptBs, ptBs)
 
 # Open the input file
 
-theInfile=ROOT.TFile("infile.root","READ")
+theInfile = ROOT.TFile("../samples/infile.root","READ")
 
 # Choose which stage of the selection you would like to see (0-17)
-theSelectionStage = 12
+theSelectionStage = 15
 
 # Get the histograms of AK8 jet mass binned by Pt of the jet
 
-hist_list = [ "AK8MPt200To300Hist" + int(theSelectionStage),    
-              "AK8MPt300To400Hist" + int(theSelectionStage),    
-              "AK8MPt400To500Hist" + int(theSelectionStage),    
-              "AK8MPt500To800Hist" + int(theSelectionStage),    
+hist_list = [ "AK8MPt200To300Hist" + str(theSelectionStage),    
+              "AK8MPt300To400Hist" + str(theSelectionStage),    
+              "AK8MPt400To500Hist" + str(theSelectionStage),    
+              "AK8MPt500To800Hist" + str(theSelectionStage),    
               ]
               
 # loop over histos and for each plot it and fit to a gaussian then save the mean              
               
 for ihisto , histo in enumerate(hist_list) :
     theHist = theInfile.Get(histo )
+    print "Now working with histogram named {}".format( histo )
     # Ensure proper error propogation
     theHist.Sumw2()
     theHist.SetDirectory(0)
@@ -43,7 +45,7 @@ for ihisto , histo in enumerate(hist_list) :
     #theHist.Scale(scaleis)
     
     # Change the number of bins from the value the histogram was initialized with
-    theHist.Rebin(5)
+    theHist.Rebin(10)
 
     # Fitting the histogram to a Gaussian
     theFitFunc = ROOT.TF1("theFitFunc", "gaus", 110, 210 )
@@ -67,14 +69,14 @@ for ihisto , histo in enumerate(hist_list) :
     
     ROOT.gStyle.SetOptFit(1111)
     c = ROOT.TCanvas('c','')
-    c.SetDirectory(0)
     
     theHist.Draw('hist')
     theFitFunc.Draw("same")
     
     # Set the maximum value for the y axis
     maxy= theHist.GetMaximum() 
-    theHist.SetMaximum( maxy * 1.618 )
+    theHist.SetMaximum( maxy * 1.2)
+    theHist.GetXaxis().SetRangeUser(80 , 250 )
 
     c.Update()
     c.Draw()
