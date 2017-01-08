4dba5ce52c73862a407a588999f815aeb5424cec
Ashley Parker <ash.marie.parker@gmail.com>
2017-01-08 14:31:32 -0600

    Update histogram saving in hist1.py

--- test/hist1.py
+++ test/hist1.py
@@ -1,20 +1,20 @@
-
-
 import ROOT
-
-theOutfile=ROOT.TFile("outfile.root","RECREATE")
-
 
 theInfile=ROOT.TFile("../samples/infile.root","READ")
 
+theOutfile=ROOT.TFile("outfile.root","RECREATE")
 
 theHist = theInfile.Get("AK8MHist16")
 
 ROOT.gStyle.SetOptFit(1111)
 theCanvas = ROOT.TCanvas('theCanvas','')
 theHist.Draw('hist')
-    
+theCanvas.Update()
+theCanvas.Draw()
+
+theHist.Write() 
+theCanvas.Write()
+   
 theOutfile.cd()
 theOutfile.Write()
 theOutfile.Close()
-
